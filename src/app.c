c9bad242b5e7f1942169ab416d475e2f2a8f7660
Syed Azeez <syedazeez337@gmail.com>
2024-01-22 20:30:47 +0530

    added welcome message function

---
+++ src/app.c
@@ -0,0 +1,12 @@
+#include "app.h"
+
+void welcomeMsg()
+{
+    printf("\n\t*************************************************\n");
+    printf("\t**     Calculates the tip                        **\n");
+    printf("\t** Format: Takes the total amount                **\n");
+    printf("\t**         Takes the tip percent                 **\n");
+    printf("\t**         Takes number of people                **\n");
+    printf("\t** Result: Gives out total amount (including tip)**\n");
+    printf("\t**         Gives out amount divided among people **\n");
+}
