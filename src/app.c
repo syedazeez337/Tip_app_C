8e419762e2c39be63c4c4c460cff4f0e8d37ea81
Syed Azeez <syedazeez337@gmail.com>
2024-01-22 20:55:34 +0530

    Added tip calculation function

--- src/app.c
+++ src/app.c
@@ -2,11 +2,34 @@
 
 void welcomeMsg()
 {
-    printf("\n\t*************************************************\n");
+    printf("\n\t***************************************************\n");
     printf("\t**     Calculates the tip                        **\n");
     printf("\t** Format: Takes the total amount                **\n");
     printf("\t**         Takes the tip percent                 **\n");
     printf("\t**         Takes number of people                **\n");
     printf("\t** Result: Gives out total amount (including tip)**\n");
     printf("\t**         Gives out amount divided among people **\n");
+}
+
+void tipCalc()
+{
+    double percent;
+    double bill;
+    int numofPeople;
+    printf("Enter bill: ");
+    scanf("%lf", &bill);
+    printf("Enter tip percent: ");
+    scanf("%lf", &percent);
+    printf("Total Number of people: ");
+    scanf("%d", &numofPeople);
+
+    double tipAmt = bill * (percent / 100);
+    double total = bill + tipAmt;
+    double eachperson = total / numofPeople;
+
+    printf("Total Bill: %.2lf\n", total);
+    printf("Tip Charged : %.2lf\n", tipAmt);
+    printf("Bill Charged to each person : %.2lf\n", eachperson);
+
+    printf("\n\n");
 }
--- src/main.c
+++ src/main.c
@@ -3,4 +3,32 @@
 int main(void)
 {
     welcomeMsg();
+
+    char choice;
+
+    bool quit = false;
+
+    while(!quit)
+    {
+        printf("Enter the programme tip(t) or quit(q): ");
+        scanf(" %c", &choice);
+
+        switch(choice)
+        {
+            case 'q' : {
+                quit = true;
+                printf("\nThank you!\n");
+                break;
+            }
+            case 't' : {
+                printf("\n\n");
+                tipCalc();
+                break;
+            }
+            default : {
+                printf("Invalid Input\n");
+                break;
+            }
+        }
+    }
 }
